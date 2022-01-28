f55d9b2d87b25f9f0acdda3605c2a43a2d6d40b5
Abril♥ <abrilmartinagil@gmail.com>
2022-01-28 15:47:51 -0300

    Update db.js

--- src/setting/db.js
+++ src/setting/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const { MONGODB_URI } = process.env
 
 /* Data base connection with Mongodb */
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
