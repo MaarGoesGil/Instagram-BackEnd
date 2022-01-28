54a9706a0f0c606979a9fddd37e552a66fdce9ce
Abril♥ <abrilmartinagil@gmail.com>
2022-01-28 20:07:56 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,12 +2,10 @@ const { Router } = require('express')
 const users = require('./users')
 const posts = require('./posts')
 const comments = require('./comments')
-const { politic } = require('../controllers/politic.js')
 const routes = Router()
 
 routes.use('/users', users)
 routes.use('/posts', posts)
 routes.use('/comments', comments)
-routes.use('/politic', politic)
 
 module.exports = routes
