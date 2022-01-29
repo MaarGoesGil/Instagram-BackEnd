bfa4aeb9ae327ce62dbb28b58ccdafe77fe54ef1
Abril♥ <abrilmartinagil@gmail.com>
2022-01-29 15:41:52 -0300

    Update posts.js

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const { authPost } = require('../middlewires/authUsers')
 const { findUserForNewPostOrComment, tokenToUserID } = require('../middlewires/findUserForNewPostOrComment')
-const { postPosts, getAllPosts, getPostsId, patchPosts, deletePosts } = require('../controllers/posts')
+const { postPosts, getAllMyPosts, getAllPosts, getPostsId, patchPosts, deletePosts } = require('../controllers/posts')
 const posts = Router()
 
 posts.post('/create', tokenToUserID, findUserForNewPostOrComment, postPosts)
-posts.get('/myposts', tokenToUserID, getAllPosts)
+posts.get('/myposts', tokenToUserID, getAllMyPosts)
 posts.get('/search', getPostsId)
+posts.get('/all', tokenToUserID, getAllPosts)
 posts.patch('/edit', authPost, patchPosts)
 posts.delete('/remove', authPost, deletePosts)
 
